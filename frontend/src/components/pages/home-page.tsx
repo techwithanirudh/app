22172d3ca95c3d4c899402ad53be8940e1867070
Tech With Anirudh <techwithanirudh@gmail.com>
2023-03-12 09:03:14 +0000

    Update home-page.scoped.css and home-page.tsx

--- frontend/src/components/pages/home-page.tsx
+++ frontend/src/components/pages/home-page.tsx
@@ -1,17 +1,17 @@
-import './home-page.scoped.css';
+import "./home-page.scoped.css";
 
-import { Link } from 'react-router-dom';
-import Navbar from '../navigation/navbar';
-import PageWrapper from './page-wrapper';
-import NormalButton from '../utils/buttons/normal-button';
-import Chat from '../../assets/home/chat.svg';
-import Devices from '../../assets/home/devices.svg';
-import Friends from '../../assets/home/friends.svg';
-import Secure from '../../assets/home/secure.svg';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { countUsers } from '../../store/users';
-import NumberFormat from 'react-number-format';
+import { Link } from "react-router-dom";
+import Navbar from "../navigation/navbar";
+import PageWrapper from "./page-wrapper";
+import NormalButton from "../utils/buttons/normal-button";
+import Chat from "../../assets/home/chat.svg";
+import Devices from "../../assets/home/devices.svg";
+import Friends from "../../assets/home/friends.svg";
+import Secure from "../../assets/home/secure.svg";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { countUsers } from "../../store/users";
+import NumberFormat from "react-number-format";
 
 const HomePage: React.FunctionComponent = () => {
   const dispatch = useDispatch();
@@ -21,81 +21,100 @@ const HomePage: React.FunctionComponent = () => {
     dispatch(countUsers());
   }, []);
 
-  const ImageCard = (props: { title: string, src: string, children?: any }) => (
-    <div className='image-card'>
+  const ImageCard = (props: { title: string; src: string; children?: any }) => (
+    <div className="image-card w-full flex flex-col items-center">
       <header className="text-center mb-2">
         <h3 className="text-white font-bold text-1xl">{props.title}</h3>
       </header>
       <img className="w-44 inline" src={props.src} alt="Chat" />
-      <footer className='text-center mt-2'>
+      <footer className="text-center mt-2">
         <div className="secondary">{props.children}</div>
       </footer>
     </div>
   );
 
   const keys: string[] = [];
-  document.addEventListener('keyup', (e) => {
+  document.addEventListener("keyup", e => {
     keys.push(e.key);
-    if (!keys.join('').endsWith('testing123')) return;
+    if (!keys.join("").endsWith("testing123")) return;
 
     try {
-      document.querySelector('h1')!.textContent = 'Easteregg++';
-      document.querySelector('.lead')!.textContent = 'Are you happy now?';
-    } catch { }
+      document.querySelector("h1")!.textContent = "Easteregg++";
+      document.querySelector(".lead")!.textContent = "Are you happy now?";
+    } catch {}
   });
 
   return (
     <PageWrapper
       className="z-10 bg-bg-tertiary h-full relative"
-      pageTitle="anichat.app | Messaging Made Simple">
+      pageTitle="anichat.app | Messaging Made Simple"
+    >
       <Navbar />
       <section className="z-10 text-center my-4">
         <h1>It's time to ditch Discord and Zoom.</h1>
         <div className="flex justify-center">
           <div className="lead font-light mt-2 max-w-xl">
-            All-in-one easy-to-use text and voice chat.
-            Stop paying for Discord boosts and hassling with Zoom.
+            All-in-one easy-to-use text and voice chat. Stop paying for Discord
+            boosts and hassling with Zoom.
             {Boolean(userCount) && (
               <span className="pl-1">
-                Join <NumberFormat
+                Join{" "}
+                <NumberFormat
                   value={userCount}
-                  displayType={'text'}
-                  thousandSeparator={true} /> Anichat users that simplified their life.
+                  displayType={"text"}
+                  thousandSeparator={true}
+                />{" "}
+                Anichat users that simplified their life.
               </span>
             )}
           </div>
         </div>
         <button className="font-primary shadow-md bg-green text-dark">
-          <Link to="/login" >
-            <NormalButton className="bg-white text-black rounded w-56 h-11 px-6 mt-8">Login</NormalButton>
+          <Link to="/login">
+            <NormalButton className="bg-white text-black rounded w-56 h-11 px-6 mt-8">
+              Login
+            </NormalButton>
           </Link>
         </button>
         <button className="shadow-md bg-green text-dark ml-10">
           <Link to="/channels/@me">
-            <NormalButton className="text-black bg-success rounded W-56 h-11 px-6 mt-8">Open Anichat in Your Browser</NormalButton>
+            <NormalButton className="text-black bg-success rounded W-56 h-11 px-6 mt-8">
+              Open Anichat in Your Browser
+            </NormalButton>
           </Link>
         </button>
       </section>
 
       <section
         title="*Description may represent unreleased features.*"
-        className="absolute top-50 w-1/2 inset-x-1/4 flex justify-between">
+        className="absolute top-50 w-1/2 inset-x-1/4 flex justify-between"
+      >
         <ImageCard src={Secure} title="Secure Messages">
-          Anichat is based on <a href={process.env.REACT_APP_REPO_URL} target="_blank">open-source software</a>.
+          Anichat is based on{" "}
+          <a href={process.env.REACT_APP_REPO_URL} target="_blank">
+            open-source software
+          </a>
+          .
         </ImageCard>
       </section>
 
       <footer className="fixed bottom-0 w-full">
-        <a href={`${process.env.REACT_APP_REPO_URL}/blob/dev/CHANGELOG.md`}
+        <a
+          href={`${process.env.REACT_APP_REPO_URL}/blob/dev/CHANGELOG.md`}
           className="float-right p-2"
-          target="none">
-          <strong className="primary">{process.env.REACT_APP_VERSION_NAME}</strong>
+          target="none"
+        >
+          <strong className="primary">
+            {process.env.REACT_APP_VERSION_NAME}
+          </strong>
           <span> </span>
-          <span className="secondary">{process.env.REACT_APP_VERSION_NUMBER}</span>
+          <span className="secondary">
+            {process.env.REACT_APP_VERSION_NUMBER}
+          </span>
         </a>
       </footer>
     </PageWrapper>
   );
-}
+};
 
 export default HomePage;
